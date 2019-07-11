45aeaf386c96e700cd59f7f235a1a4353baca5fa
Santiago San Román <santi72alc@gmail.com>
2019-07-12 01:27:32 +0200

    La mano son objetos de carta

--- kata3.mjs
+++ kata3.mjs
@@ -44,41 +44,35 @@ Valor:
 
 #### Mano
 
-Una mano es un conjunto de 5 cartas, estamos jugando con una baraja, por lo que no puede haber cartas 
-repetidas.
+Una mano es un conjunto de 5 cartas, estamos jugando con una baraja, por lo que no puede haber cartas repetidas.
 
 Las manos de poker se ordenan de menor a mayor dependiendo de una serie de reglas asociadas a la mano.
 
-* High Card (Carta Más Alta): Para manos que no entran en ninguna de las manos superior, 
-el ganador es aquel que tiene la carta más alta. Si se produce un empate entonces se compara 
-la siguiente carta más alta y así sucesivamente.
+* High Card (Carta Más Alta): Para manos que no entran en ninguna de las manos superior, el ganador es aquel que tiene 
+la carta más alta. Si se produce un empate entonces se compara la siguiente carta más alta y así sucesivamente.
 
-* Pair (Parejas): 2 de las 5 cartas de la mano tienen el mismo valor. Si las dos manos tienen pareja, 
-entonces gana la que tenga la pareja más alta. Si ambas parejas son iguales entonces gana el que tenga 
+* Pair (Parejas): 2 de las 5 cartas de la mano tienen el mismo valor. Si las dos manos tienen pareja, entonces gana 
+la que tenga la pareja más alta. Si ambas parejas son iguales entonces gana el que tenga la carta más alta.
+
+* Two Pairs (Dobles Parejas): La mano contiene 2 parejas diferentes. Si las dos manos tienen dobles parejas diferentes 
+entonces gana aquella que tenga la pareja más alta. Si las dos manos tienen las mismas parejas entonces se compara la otra
+ pareja. Si ambas manos tiene las mismas parejas entonces gana el que tenga la carta más alta restante.
+
+* Three of a Kind (Trio): 3 cartas de la mano tienen el mismo valor. Gana la mano que tiene las 3 cartas con mayor valor.
+
+* Straight (Escalera): La mano contiene 5 cartas consecutivas. Si las dos manos tienen escalera entonces gana la que tiene 
 la carta más alta.
 
-* Two Pairs (Dobles Parejas): La mano contiene 2 parejas diferentes. Si las dos manos tienen dobles parejas 
-diferentes entonces gana aquella que tenga la pareja más alta. Si las dos manos tienen las mismas parejas 
-entonces se compara la otra pareja. Si ambas manos tiene las mismas parejas entonces gana el que tenga la 
-carta más alta restante.
+* Flush (Color): La mano tiene 5 cartas con la misma cara. Si ambas manos tienen color entonces gana el que tenga la carta 
+más alta.
 
-* Three of a Kind (Trio): 3 cartas de la mano tienen el mismo valor. Gana la mano que tiene las 3 cartas 
-con mayor valor.
+* Full House (Full): La mano tiene un trío y una pareja. En caso de tener ambas manos full entonces gana el que tenga el 
+trío más alto.
 
-* Straight (Escalera): La mano contiene 5 cartas consecutivas. Si las dos manos tienen escalera entonces 
-gana la que tiene la carta más alta.
+* Four of a Kind (Poker): 4 cartas con el mismo valor. En caso de tener ambas manos poker gana el que tenga el valor más alto.
 
-* Flush (Color): La mano tiene 5 cartas con la misma cara. Si ambas manos tienen color entonces gana el 
-que tenga la carta más alta.
-
-* Full House (Full): La mano tiene un trío y una pareja. En caso de tener ambas manos full entonces gana 
-el que tenga el trío más alto.
-
-* Four of a Kind (Poker): 4 cartas con el mismo valor. En caso de tener ambas manos poker gana el que tenga 
-el valor más alto.
-
-* Straight flush (Escalera de Color): 5 cartas de la misma cara pero con valores consecutivos. En caso de 
-tener escalera las dos manos entonces gana el que tenga el valor más alto.
+* Straight flush (Escalera de Color): 5 cartas de la misma cara pero con valores consecutivos. En caso de tener escalera las 
+dos manos entonces gana el que tenga el valor más alto.
 
 #### Ejemplos
 
@@ -94,90 +88,156 @@ Salida: Jugador 1 gana, carta más alta
 Entrada: Jugador 1: 2H 3D 5S 9C KD Jugador 2: 2D 3H 5C 9S KH
 Salida: Empate
 
+
 */
 /* ************************************************ */
 
 // Variables públicas para el juego
-const suitValues = {
+const suitNames = {
     S: "Picas",
     H: "Corazones",
     C: "Tréboles",
     D: "Diamantes"
 };
-const valorValues = {
+const codeValues = {
     // "01": "As",
-    "02": "2",
-    "03": "3",
-    "04": "4",
-    "05": "5",
-    "06": "6",
-    "07": "7",
-    "08": "8",
-    "09": "9",
-    "10": "Diez",
-    "11": "Dama",
-    "12": "Reina",
-    "13": "Rey",
-    "14": "As"
+    "2" : 2,
+    "3" : 3,
+    "4" : 4,
+    "5" : 5,
+    "6" : 6,
+    "7" : 7,
+    "8" : 8,
+    "9" : 9,
+    "T" : 10,
+    "J" : 11,
+    "Q" : 12,
+    "K" : 13,
+    "A" : 14
 };
 
+const codeNames = {
+    "2" : "2",
+    "3" : "3",
+    "4" : "4",
+    "5" : "5",
+    "6" : "6",
+    "7" : "7",
+    "8" : "8",
+    "9" : "9",
+    "10": "Diez",
+    "11" : "Dama",
+    "12" : "Reina",
+    "13" : "Rey",
+    "14" : "As"
+}
+
+let fullBaraja = [];
 let usedValues = [];        // Almacena las cartas que tienen los jugadores. Para evitar repetidas!!
+
 
 // Clase Carta
 class Carta {
-    constructor(suit = "", valor = "") {
+    constructor( code = "" ) {
+
         // propiedades
-        this.suit =
-            suit != "" ? this.getSuit(suit.toString()) : this.getRandomSuits();
-        this.valor =
-            valor != "" ? this.getValor(valor.toString()) : this.getRandomValor();
+        let resp = this.readCode( code );
+        this.value = resp[0];
+        this.suit = resp[1];
+        this.code = this.makeCode();    // creamos el código de la carta
     }
+
+
+    readCode( code ) {
+        let value;
+        let suit;
+        suit = code.slice( 1 );
+        value = code.slice( 0, 1);
+
+        switch (value) {
+            case "A": value = 14; break;
+            case "K": value = 13; break;
+            case "Q": value = 12; break;
+            case "J": value = 11; break;
+            case "T": value = 10; break;
+            case "9": value = 9; break;
+            case "8": value = 8; break;
+            case "7": value = 7; break;
+            case "6": value = 6; break;
+            case "5": value = 5; break;
+            case "4": value = 4; break;
+            case "3": value = 3; break;
+            case "2": value = 2; break;
+        }
+        return [parseInt(value), suit];
+    }
+
+
+    makeCode() {
+        let value = '';
+
+        switch (this.value.toString()) {
+            case "14": value = 'A'; break;
+            case "13": value = 'K'; break;
+            case "12": value = 'Q'; break;
+            case "11": value = 'J'; break;
+            case "10": value = 'T'; break;
+            case "9": value = "9"; break;
+            case "8": value = "8"; break;
+            case "7": value = "7"; break;
+            case "6": value = "6"; break;
+            case "5": value = "5"; break;
+            case "4": value = "4"; break;
+            case "3": value = "3"; break;
+            case "2": value = "2"; break;
+        }
+        return `${value}${this.suit}`;
+    }
+
+
 
     // métodos
     // devuelve el 'suit - palo' que tenga la carta
     getSuit(suit = this.suit) {
-        if (suit != "" && suit in suitValues) {
+        if (suit != "" && suit in suitNames) {
             this.suit = suit;
         }
         return this.suit;
     }
 
-    // Devuelve el valor que tenga la carta, si se le pasa "" genera uno aleatorio.
-    getValor(valor = this.valor) {
-        if (valor != "" && valor in valorValues) {
-            this.valor = valor;
+    // Devuelve el valor que tenga la carta
+    getValue(value = "") {
+        if (value != "" && value in codeValues) {
+            value = codeValues[value].toString();
         }
-        return this.valor;
+        else { value = ""; }
+        return value;
     }
 
     // Buscamos un valor aletario para 'suit - palo'
-    getRandomSuits() {
+    getRandomSuit() {
         let min = 0;
-        let max = Object.keys(suitValues).length - 1;
-        let suit = Object.keys(suitValues)[
+        let max = Object.keys(suitNames).length - 1;
+        let index = Object.keys(suitNames)[
             Math.floor(Math.random() * (max - min)) + min
         ];
-        return suit;
+        return index;
     }
 
     // Buscamos un valor aletario para 'valor - valor'
-    getRandomValor() {
+    getRandomValue() {
         let min = 0;
-        let max = Object.keys(valorValues).length - 1;
-        let valor = Object.keys(valorValues)[
+        let max = Object.keys(codeValues).length - 1;
+        let index = Object.keys(codeValues)[
             Math.floor(Math.random() * (max - min)) + min
         ];
-        return valor;
+        return index;
     }
 
-    // Nos devuelve string de DATO con el par 'suit/valor' - 'palo/valor'
-    data() {
-        return `${this.suit}${this.valor}`;
-    }
-
-    // Nos devuelve string con el par 'suit/valor' - 'palo/valor'
+    // Nos devuelve string decorado del par 'suit/value' - 'palo/valor'
     info() {
-        return `${valorValues[this.valor]} de ${suitValues[this.suit]}`;
+
+        return `${codeNames[this.value.toString()]} de ${suitNames[this.suit]}`;
     }
 }
 /* FIN declaración de class Carta ********************************************* */
@@ -185,28 +245,28 @@ class Carta {
 
 
 class Baraja {
-    constructor(fill = true) {
-        this.values = (fill) ? this.fillBaraja() : [];
+    constructor() {
+        this.values = this.fillBaraja();
     }
 
     // Rellenamos la bajara con las 52 cartas posibles
-    fillBaraja() {
+    fillBaraja () {
         let baraja = [];
         // Rellenamos la baraja
-        for (let suit in suitValues) {
+        // cada 'valor' de la carta
+        for (let value in codeValues) {
             // Cada 'suit/palo'
-            for (let valor in valorValues) {
-                // cada 'valor' de la carta
-                baraja.push(new Carta(suit, valor));
+            for (let suit in suitNames) {
+                baraja.push(new Carta(`${value}${suit}`));
             }
         }
         return baraja;
     }
 
-    existCarta(suit, valor) {
+    existCarta( code ) {
         let exist = false;
         this.values.forEach(element => {
-            if (element.data() == `${suit}${valor}`) {
+            if (element.code == code) {
                 exist = true;
             }
         });
@@ -221,57 +281,67 @@ class Baraja {
 /* FIN declaración de class Baraja ********************************************* */
 
 
+class Jugador  {
+    // El jugador consta de [ 'nombre' , [array 5 cartas] ]. Si no se pasan las cartas, se cogen aleatorias
+    constructor(jugador = ["Jugador", []]) {
+        // super( (typeof jugador[1] == 'array' && jugador[1].length == 5) ? jugador[1] : [] );              // Inicializamos jugador con la mano en blanco (sin cartas)
 
-class Jugador extends Baraja {
-    constructor(name = "") {
-        super(false);   // Inicializamos jugador con la mano en blanco (sin cartas)
-
-        this.name = name == "" ? "Jugador" : name;
-        this.hand = [];         // Mano del jugador (5 cartas diferentes de la baraja)
+        this.name = (typeof jugador[0] != 'string' || jugador[0] == "") ? "Jugador" : jugador[0];
+        // Mano del jugador (5 cartas diferentes de la baraja)
+        this.hand = [];         // Mano del jugador (5 cartas)
         this.totalHand = 0;     // Total de puntos del jugador en la mano actual
         this.totalTrio = 0;     // Total de puntos del trio (si existe en la mano actual, sino será 0)
 
-        // Rellenamos la mano del jugador
-        this.fillHand();
+        (typeof jugador[1] == 'object' && jugador[1].length == 5) ? this.fillHand(jugador[1]) : this.fillHand([]);  
+
     }
 
-    // Rellenamos la mano del jugador con 5 cartas aleatorias()
-    fillHand() {
-        this.hand = [];
+    // Rellenamos la mano del jugador con 5 cartas definidas o aleatorias()
+    fillHand( hand = [] ) {
         let carta;
         let exist = false;
+        let objHand = [];
 
-        for (let i = 0; this.hand.length < 5; i++) {
+        if (hand != []) {       // Cada elemento del array pasado, lo convertimos a class 'Carta'
+            hand.forEach( element => {
+                carta = new Carta(element.toUpperCase());
+                objHand.push(carta);
+            });
+        }
+
+        for (let i = 0; this.hand.length < 5; i++) {        // SIEMPRE leemos/creamos 5 cartas para la mano
             exist = false;
-            carta = new Carta();
 
             // Miramos si existe en las cartas ya guardados
-            usedValues.forEach( element => { 
-                if (element.data() == carta.data()) { exist = true; }
+            objHand.forEach( carta => {
+                usedValues.forEach( element => { 
+                    if (element.code == carta.code) { exist = true; }
+                });
+                if (!exist) {   // La carta NO está usada... la añadimos en su mano y en 'usadas'
+                    this.hand.push(carta); 
+                    usedValues.push(carta);
+                } 
+                else { objHand.push(new Carta()); }       // Añadimos una aleatoria ???
             });
-            if (!exist) {   // La carta NO está usada... la añadimos
-                this.hand.push(carta); 
-                usedValues.push(carta);
-            }
         }
         this.sortHand();    // Ordenamos la mano del jugador
         this.totalHand = 0;
         this.hand.forEach( element => {     // Calculamos el total de la mano del jugador
-            this.totalHand += parseInt(element.valor);
+            this.totalHand += parseInt(element.value);
         }); 
     }
 
     // Ordena la mano del jugador
     sortHand () {
         this.hand.sort( function( a, b ) {
-            return b.data().toString().localeCompare( a.data().toString() );
+            return b.code.localeCompare( a.code );
         });
     }
 
     // Visualiza los datos del jugador y su mano actual
     showHand() {
-        console.log(`Jugador : ${this.name}, con una mano de ${this.totalHand} puntos\n`);
-        console.log(`artas de la mano... (máx. valor carta ${this.highestHandValue().toString()})`);
+        console.log(`JUGADOR => ${this.name}, con una mano de ${this.totalHand} puntos\n`);
+        console.log(`Mano... (carta de máx. valor ${this.highestHandValue()})`);
         this.hand.forEach(element => {
             console.log(element.info());
         });
@@ -283,8 +353,11 @@ class Jugador extends Baraja {
     // Devuelve el valor más alto de la mano
     highestHandValue () {
         let values =  [];
-        this.hand.forEach( element => { values.push( element.valor ); });
-        return Math.max( ...values );
+        this.hand.forEach( element => { 
+            values.push( element.value );           
+        });
+        // return values[0];
+        return  Math.max( ...values );
     }
 
     // Devuelve si tenemos EXACTAMENTE 3 valores iguales en la mano
@@ -292,7 +365,7 @@ class Jugador extends Baraja {
         let times = [];
         let pos = 0;
         this.hand.forEach( element => {
-            pos = element.valor;
+            pos = element.value;
             if ( times[ pos ] == undefined ) { times[ pos ] = 1; }
             else { times[ pos ]++; }
         });
@@ -307,14 +380,12 @@ class Jugador extends Baraja {
         let times = [];
         let pos = 0;
         this.hand.forEach( element => {
-            pos = element.valor;
+            pos = element.value;
             if ( times[ pos ] == undefined ) { times[ pos ] = 1 }
             else { times[ pos ]++; }
         });
         return times.includes(2);
     }
-
-
 
     // Devuelve si el jugador tiene 'Stright_Flush' / 'Escalera de Color'
     isStraightFlush () {
@@ -326,12 +397,12 @@ class Jugador extends Baraja {
         this.hand.forEach( element => {
             if (oldSuit == "") {        // El primer elemento
                 oldSuit = element.suit;
-                min = max = element.valor;
+                min = max = element.value;
             }
             else if ( element.suit != oldSuit || !isStraight ) { isStraight = false; }     // NO es del mismo palo
             else {
-                if (element.valor == min - 1) { min = element.valor; }
-                else if (element.valor == max + 1) { max = element.valor; }
+                if (element.value == min - 1) { min = element.value; }
+                else if (element.value == max + 1) { max = element.value; }
                 else { isStraight = false; }    // NO es valor consecutivo
             }
         });
@@ -344,8 +415,8 @@ class Jugador extends Baraja {
         let oldValor = 0;
         let cont = 0;
         this.hand.forEach( element => {
-            if (cont == 0) { oldValor = element.valor }         // Cogemos el 1er valor
-            else if (element.valor != oldValor ) { cont++; }
+            if (cont == 0) { oldValor = element.value }         // Cogemos el 1er valor
+            else if (element.value != oldValor ) { cont++; }
             if (cont > 2) { isFour = false; }       // Más de 2 cartas diferentes NO puede haber poker
         });
         return isFour;
@@ -366,7 +437,8 @@ class Jugador extends Baraja {
 
 
 class Game {
-    constructor (player1 = "Jugador 2", player2 = "Jugador 2") {
+    constructor (player1 = ["Jugador 1", []], player2 = ["Jugador 2", []]) {
+        fullBaraja = new Baraja();
         this.jugador1 = new Jugador(player1);
         this.jugador2 = new Jugador(player2);
     }
@@ -402,7 +474,10 @@ class Game {
 
 
 export function main() {
-    let game = new Game("Santi", "Machine");
+    let game = new Game(
+        ["Santi",       ['2H', '3D', '5S', '9C', 'KD']],
+        ["Machine",     ['2c', '3H', '4S', '8C', 'AH']]
+        );
 
     game.play();
 
